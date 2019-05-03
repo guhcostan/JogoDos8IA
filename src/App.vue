55ca79a0abe8bbed46352719184186ec82729e24
Gustavo Costa <gustavo.neves.lemaf@gmail.com>
2019-05-03 15:16:57 -0300

    Adding files.

--- src/App.vue
+++ src/App.vue
@@ -4,7 +4,7 @@
 		p(v-else) Carregando ...
 
 		p(v-if="melhorJogada.numeroJogadas") Passo: {{matriz.numero}}
-		.jogo
+		.jogo(v-if="melhorJogada.numeroJogadas")
 			.linha(v-for="linha in matriz")
 				.bloco(v-for="bloco in linha" :class="{player: bloco === 0}")
 					p {{bloco}}
@@ -48,33 +48,68 @@ export default {
 </script>
 
 <style lang="scss">
+	$--color-primary: #8844EE;
+	$--color-text: #C2C6D3;
+	$--color-bg: #26353E;
+
+	@import url('https://fonts.googleapis.com/css?family=Raleway');
+
+	html, body {
+		background-color: $--color-bg;
+		width: 100%;
+		height: 95%;
+		padding: 0;
+		margin: 0;
+	}
+
 	#app {
-		font-family: 'Avenir', Helvetica, Arial, sans-serif;
+		font-family: 'Raleway', Helvetica, Arial, sans-serif;
 		-webkit-font-smoothing: antialiased;
 		-moz-osx-font-smoothing: grayscale;
 		text-align: center;
-		color: #2c3e50;
-		margin-top: 60px;
+		color: $--color-text;
+		background-color: $--color-bg;
+		width: 100%;
+		height: 100%;
+		font-size: 28px;
 
 
 		.jogo{
-			width: fit-content;
 			margin: 0 auto;
+			width: 40%;
+			height: 60%;
+			background-color: white;
+			border-radius: 4px;
+			box-shadow: 3px 3px 5px rgba(0, 0, 0, 0.2);
+			color: black;
+			border: $--color-primary 10px solid;
 		}
 		.linha{
 			display: flex;
+			height: 33.3333333%;
+			justify-content: center;
+			align-items: center;
 
 
 			.bloco{
-				border: solid black;
-				width: 40px;
+				width: 33.33333%;
+				height: 100%;
+				display: flex;
+				align-items: center;
+				justify-content: center;
+				font-size: 28px;
+				border: rgba(194, 198, 211, 0.42) 2px solid;
 			}
 		}
 
 		.player{
 			margin: auto 0;
-			height: 100%;
-			background-color: red;
+			background-color: $--color-primary;
+			color: white;
+			font-weight: bold;
+			font-size: 50px !important;
+			border: $--color-bg 5px solid !important;
+			z-index: 1000;
 		}
 	}
 </style>
